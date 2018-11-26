99784b4d5a5e7ad1cee231860b55aba836ec71a6
mayton <mark.kovalyov78@gmail.com>
2018-11-26 09:52:16 +0200

    Add some lines

--- ProbeJena/src/main/java/mayton/probe/Main.java
+++ ProbeJena/src/main/java/mayton/probe/Main.java
@@ -1,8 +1,22 @@
 package mayton.probe;
 
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.util.FileManager;
+
 public class Main {
 
+    /**
+     * http://www.ldodds.com/foaf/foaf-a-matic.html
+     *
+     */
+
+
     public static void main(String[] args) {
+
+        FileManager.get().addLocatorClassLoader(Main.class.getClassLoader());
+        Model model = FileManager.get().loadModel("in/main.rdf", "http://mayton.org", null);
+
+        model.write(System.out, "TURTLE");
 
 
 
