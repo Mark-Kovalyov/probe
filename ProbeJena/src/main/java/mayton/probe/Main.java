f5663705272d8e7b7ad1caa14cb86770bef181bc
mayton <mark.kovalyov78@gmail.com>
2018-11-26 13:28:46 +0200

    Changes

--- ProbeJena/src/main/java/mayton/probe/Main.java
+++ ProbeJena/src/main/java/mayton/probe/Main.java
@@ -1,6 +1,9 @@
 package mayton.probe;
 
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.util.FileManager;
 
 public class Main {
@@ -14,11 +17,31 @@ public class Main {
     public static void main(String[] args) {
 
         FileManager.get().addLocatorClassLoader(Main.class.getClassLoader());
-        Model model = FileManager.get().loadModel("in/main.rdf", "http://mayton.org", null);
+
+        Model model = FileManager.get().loadModel("input/main.xml", "http://mayton.org", null);
 
         model.write(System.out, "TURTLE");
 
+        String queryString =
+                "PREFIX rdf: <http://address.com/#> \n" +
+                "SELECT * FROM { \n" +
+                " ?s ?p ?o . \n" +
+                "}\n";
 
+        Query query = QueryFactory.create(queryString);
+
+        QueryExecution execution = QueryExecutionFactory.create(query);
+
+        ResultSet resultSet = execution.execSelect();
+
+        while(resultSet.hasNext()) {
+            QuerySolution querySolution = resultSet.nextSolution();
+
+            Resource res = querySolution.getResource("res");
+            Literal  lit = querySolution.getLiteral("lit");
+        }
+
+        execution.close();
 
     }
 }
