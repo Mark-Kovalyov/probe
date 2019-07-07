7a1053c42a5f13f80fa4e05ce35447df536240ae
mayton <mayton@gmail.com>
2019-07-07 23:10:54 +0300

    Add Apache Hadoop demos

---
+++ probe-apache-hadoop/docs/hadoop.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1562014274383" ID="ID_1362184661" MODIFIED="1562014274383" TEXT="New Mindmap"/>
+</map>
