0827802cc7372af41552cd1b9654cc801729e0ca
mayton <mark.kovalyov78@gmail.com>
2019-07-22 22:08:08 +0300

    Add calcite

---
+++ probe-calcite/src/main/java/mayton/probe/Main.java
@@ -0,0 +1,14 @@
+package mayton.probe;
+
+import org.apache.calcite.avatica.AvaticaConnection;
+
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Connection connection = null;
+
+    }
+
+}
