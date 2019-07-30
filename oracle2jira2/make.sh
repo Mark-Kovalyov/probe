0d951fbd78c15fc7cfca214f02bf7831c22fda53
mayton <mark.kovalyov78@gmail.com>
2019-07-30 21:26:33 +0300

    Drop Oracle2Jira

--- oracle2jira2/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mvn clean package
-
-cp ./target/*.jar .
-
-java -XX:+UseSerialGC -jar SQL2Horizontal.jar "scott/tiger@127.0.0.1:1521/XE" "select * from emp order by 1"
-
-java -XX:+UseSerialGC -jar SQL2Vertical.jar   "scott/tiger@127.0.0.1:1521/XE" "select * from emp order by 1"
