25a25fd5ef549100a8924b9fe04a626c4d665ebc
mayton <mark.kovalyov78@gmail.com>
2019-12-18 09:29:54 +0200

    Add POW

--- probe-pow/src/main/java/mayton/crypto/Main.java
+++ probe-pow/src/main/java/mayton/crypto/Main.java
@@ -1,6 +1,9 @@
 package mayton.crypto;
 
 import org.apache.commons.codec.binary.Hex;
+import org.apache.hadoop.util.bloom.BloomFilter;
+import org.apache.hadoop.util.bloom.Key;
+import org.apache.hadoop.util.hash.Hash;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -15,6 +18,8 @@ import java.util.zip.GZIPInputStream;
 public class Main {
 
     // Facts:
+    // ======
+    //
     // Size gzippped = 8G
     // Size = ?
     // Rows = 1 206 769 972
@@ -25,6 +30,9 @@ public class Main {
     // Bloom Size : 0.7 * m = n
     // m = 1206769972 keys
     // n = 0.7 * 1206769972 = 844738980 bits = 105592372 bytes = 100 Mb
+    //
+    // Aftera hashing:
+    //
 
     public static void main(String[] args) throws Exception {
         GZIPInputStream inputStream = new GZIPInputStream(new FileInputStream("/db/1.2billion/1.2billion.txt.gz"));
@@ -32,15 +40,25 @@ public class Main {
         String line = null;
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
         Pattern pattern = Pattern.compile("[A-Za-z0-9]+");
-        int maxRows = 10000;
+        int maxRows = 100_000_000;
         int ignoredRows = 0;
         int current = 0;
+        int bloomDuplicates = 0;
+        // TODO calculate!
+        int nbHash = 13;
+        BloomFilter bloomFilter = new BloomFilter(1_206_769_972, nbHash, Hash.MURMUR_HASH);
         while ((line = bufferedReader.readLine()) != null && current < maxRows) {
             if (pattern.matcher(line).matches()) {
                 messageDigest.reset();
                 messageDigest.update(line.getBytes(StandardCharsets.UTF_8));
                 byte[] result = messageDigest.digest();
-                System.out.printf("%s %s\n", line, Hex.encodeHexString(result));
+                //System.out.printf("%s %s\n", line, Hex.encodeHexString(result));
+                Key key = new Key(result);
+                if (bloomFilter.membershipTest(key)) {
+                    System.err.printf("Bloom collision detected on key = %s", Hex.encodeHexString(result));
+                    bloomDuplicates++;
+                }
+                bloomFilter.add(key);
             } else {
                 ignoredRows++;
             }
