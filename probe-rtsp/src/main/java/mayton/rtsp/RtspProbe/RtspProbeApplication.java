71f5f722d49bf1ece580423b298d57ffb019279c
mayton <mark.kovalyov78@gmail.com>
2020-04-12 20:12:46 +0300

    Add RTSP test project

---
+++ probe-rtsp/src/main/java/mayton/rtsp/RtspProbe/RtspProbeApplication.java
@@ -0,0 +1,13 @@
+package mayton.rtsp.RtspProbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtspProbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtspProbeApplication.class, args);
+	}
+
+}
