b2adf3797950980975fc777bfa02c3062286917a
mayton <mark.kovalyov78@gmail.com>
2020-04-12 19:06:57 +0300

    Add XSD test project

--- probe-xsd/src/main/java/mayton/xml/Main.java
+++ probe-xsd/src/main/java/mayton/xml/Main.java
@@ -1,9 +1,5 @@
 package mayton.xml;
 
-import org.apache.commons.compress.compressors.CompressorException;
-import org.apache.commons.compress.compressors.CompressorInputStream;
-import org.apache.commons.compress.compressors.CompressorStreamFactory;
-
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -11,23 +7,15 @@ import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 import java.io.*;
+import java.util.zip.GZIPInputStream;
 
 public class Main {
 
-    public static BufferedReader getBufferedReaderForCompressedFile(String path) throws FileNotFoundException, CompressorException {
-        FileInputStream fin = new FileInputStream(path);
-        BufferedInputStream bis = new BufferedInputStream(fin);
-        CompressorInputStream compressorInputStream = new CompressorStreamFactory().createCompressorInputStream(bis);
-        BufferedReader br2 = new BufferedReader(new InputStreamReader(compressorInputStream));
-        return br2;
-    }
-
-    public static void main(String[] args) throws FileNotFoundException, XMLStreamException, CompressorException {
+    public static void main(String[] args) throws IOException, XMLStreamException {
         XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
 
-        CompressorInputStream compressorInputStream = new CompressorStreamFactory().createCompressorInputStream(new FileInputStream("/bigdata/open-street-map/changesets-latest.osm.bz2"));
-
-        XMLEventReader reader = xmlInputFactory.createXMLEventReader(compressorInputStream);
+        GZIPInputStream gzipInputStream = new GZIPInputStream(new FileInputStream("/bigdata/open-street-map/changesets-latest.osm.gz"));
+        XMLEventReader reader = xmlInputFactory.createXMLEventReader(gzipInputStream);
         while (reader.hasNext()) {
             XMLEvent nextEvent = reader.nextEvent();
             if (nextEvent.isStartElement()) {
@@ -37,10 +25,10 @@ public class Main {
             }
             if (nextEvent.isEndElement()) {
                 EndElement endElement = nextEvent.asEndElement();
-
                 System.out.print("<");
                 System.out.println(endElement.getName());
             }
+
         }
 
 
---
+++ probe-xsd/src/main/java/mayton/xml/Validate.java
@@ -0,0 +1,45 @@
+package mayton.xml;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+public class Validate {
+
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+        // parse an XML document into a DOM tree
+        DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = parser.parse(new File("instance.xml"));
+
+        // create a SchemaFactory capable of understanding WXS schemas
+        SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+        // load a WXS schema, represented by a Schema instance
+        Source schemaFile = new StreamSource(new File("mySchema.xsd"));
+        Schema schema = factory.newSchema(schemaFile);
+
+        // create a Validator instance, which can be used to validate an instance document
+        Validator validator = schema.newValidator();
+
+        // validate the DOM tree
+        try {
+            validator.validate(new DOMSource(document));
+        } catch (SAXException e) {
+            // instance document is invalid!
+        }
+    }
+
+
+}
