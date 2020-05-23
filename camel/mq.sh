c49ce7f8b23b430769d6d437b007f1274d5abf04
mayton <mark.kovalyov78@gmail.com>
2020-05-23 14:32:09 +0300

    Remove 99% of Camel examples

--- camel/mq.sh
+++
@@ -1,8 +0,0 @@
-mvn archetype:generate ^
-  -DarchetypeGroupId=org.apache.camel.archetypes ^
-  -DarchetypeArtifactId=camel-archetype-activemq ^
-  -DarchetypeRepository=https://repository.apache.org/content/groups/snapshots-group ^
-  -DinteractiveMode=false ^
-  -DgroupId=mayton ^
-  -DartifactId=camelArchetypeActiveMqExample ^
-  -Dpackage=camel
--- camel/web.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -v
-
-mvn archetype:generate \
-  -DarchetypeGroupId=org.apache.camel.archetypes \
-  -DarchetypeArtifactId=camel-archetype-web \
-  -DarchetypeRepository=https://repository.apache.org/content/groups/snapshots-group \
-  -DinteractiveMode=false \
-  -DgroupId=mayton \
-  -DartifactId=camelArchetypeWebExample 
