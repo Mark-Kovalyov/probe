9764a18e096339eb46631002c5c276361181d3f6
mayton <mark.kovalyov78@gmail.com>
2020-06-29 18:21:01 +0300

    Add rutorparse

---
+++ probe-html-parser/rutorparse/src/main/java/mayton/parsers/rutorparse/RutorparseApplication.java
@@ -0,0 +1,13 @@
+package mayton.parsers.rutorparse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutorparseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutorparseApplication.class, args);
+	}
+
+}
---
+++ probe-html-parser/rutorparse/src/test/java/mayton/parsers/rutorparse/RutorparseApplicationTests.java
@@ -0,0 +1,13 @@
+package mayton.parsers.rutorparse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutorparseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
