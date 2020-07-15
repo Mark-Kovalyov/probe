d9ebf4db7ea9a7a392594e6281cf775e666ef93d
mayton <mark.kovalyov78@gmail.com>
2020-07-15 19:48:34 +0300

    Add project JWT-command-line-interface

---
+++ probe-jwt-cli/src/main/java/mayton/network/JwtCli.java
@@ -0,0 +1,17 @@
+package mayton.network;
+import com.nimbusds.jwt.SignedJWT;
+import org.apache.commons.io.IOUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+
+public class JwtCli {
+
+    public static void main(String[] args) throws IOException, ParseException {
+        SignedJWT signedJWT = SignedJWT.parse((IOUtils.toString(new FileInputStream(args[0]), StandardCharsets.UTF_8)));
+        System.out.println(signedJWT.getJWTClaimsSet());
+    }
+
+}
