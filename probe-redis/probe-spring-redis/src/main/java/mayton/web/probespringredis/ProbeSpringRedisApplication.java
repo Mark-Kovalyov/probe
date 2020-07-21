0b0ac3b8e95060150e7f1deb4ce65a1dce4c4f15
mayton <mark.kovalyov78@gmail.com>
2020-07-22 00:35:43 +0300

    Add probe-spring-redis

---
+++ probe-redis/probe-spring-redis/src/main/java/mayton/web/probespringredis/ProbeSpringRedisApplication.java
@@ -0,0 +1,13 @@
+package mayton.web.probespringredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbeSpringRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbeSpringRedisApplication.class, args);
+	}
+
+}
---
+++ probe-redis/probe-spring-redis/src/test/java/mayton/web/probespringredis/ProbeSpringRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package mayton.web.probespringredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbeSpringRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
