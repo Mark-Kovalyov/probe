7838f27d51a0308841038bbc4ef83fec47211a70
Mark.Kovalyov <mark.kovalyov78@gmail.com>
2020-09-15 22:50:55 +0300

    Drop probe-compression

--- probe-compression/go.cmd
+++
@@ -1,8 +0,0 @@
-export LIMIT=100
-export SELECTION=ORDERED
-
-call mvn clean package
-
-mkdir trie
-
-java -jar target\probe-compression-1.0-SNAPSHOT.jar %LIMIT% %SELECTION%
--- probe-compression/src/main/java/mayton/compression/Lhm.java
+++
@@ -1,7 +0,0 @@
-package mayton.compression;
-
-import java.util.LinkedHashMap;
-
-public class Lhm extends LinkedHashMap<String, Object> {
-    // Just datatype
-}
