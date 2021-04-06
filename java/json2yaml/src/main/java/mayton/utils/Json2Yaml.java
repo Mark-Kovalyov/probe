0c67921636e975a5a1101b1cbfd47f4ca86a1f5a
Mark.Kovalyov <mark.kovalyov78@gmail.com>
2021-04-06 21:41:25 +0300

    Add Json2Yaml

---
+++ java/json2yaml/src/main/java/mayton/utils/Json2Yaml.java
@@ -0,0 +1,14 @@
+package mayton.utils;
+
+import java.io.*;
+
+/**
+ * Hello world!
+ */
+public class Json2Yaml {
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream is = new FileInputStream(args[0]);
+        OutputStream os = new FileOutputStream(args[1]);
+
+    }
+}
