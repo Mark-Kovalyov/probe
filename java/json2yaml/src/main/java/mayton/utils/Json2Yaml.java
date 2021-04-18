4e399d551339986568c39c6e0e7ff04adfc2045e
Mark.Kovalyov <mark.kovalyov78@gmail.com>
2021-04-19 00:49:57 +0300

    Add project JSON-2-YAML

--- java/json2yaml/src/main/java/mayton/utils/Json2Yaml.java
+++ java/json2yaml/src/main/java/mayton/utils/Json2Yaml.java
@@ -1,14 +1,21 @@
 package mayton.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
 import java.io.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-/**
- * Hello world!
- */
 public class Json2Yaml {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         InputStream is = new FileInputStream(args[0]);
-        OutputStream os = new FileOutputStream(args[1]);
-
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String,Object> result = objectMapper.readValue(is, LinkedHashMap.class);
+        DumperOptions dumperOptions = new DumperOptions();
+        dumperOptions.setIndent(4);
+        dumperOptions.setAllowUnicode(true);
+        Yaml yaml = new Yaml(dumperOptions);
+        yaml.dump(result, new FileWriter(args[1]));
     }
 }
