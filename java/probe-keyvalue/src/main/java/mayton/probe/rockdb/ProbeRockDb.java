940bb15451739d79811ae5d46600c278a43e4133
Mark <mark.kovalyov78@gmail.com>
2021-04-19 23:50:30 +0300

    Changes in probe-key-val

--- java/probe-keyvalue/src/main/java/mayton/probe/rockdb/ProbeRockDb.java
+++ java/probe-keyvalue/src/main/java/mayton/probe/rockdb/ProbeRockDb.java
@@ -2,12 +2,25 @@ package mayton.probe.rockdb;
 
 import org.rocksdb.*;
 
+import java.io.File;
+
 public class ProbeRockDb {
 
     public static void main(String[] args) throws RocksDBException {
 
+        for(File f : new File("/storage/fias/fias_dbf").listFiles()) {
+            if (!f.isDirectory()) {
+                String fileName = f.getName();
+                if (fileName.endsWith(".DBF")) {
+                    System.out.println(fileName);
+                }
+            }
+        }
+
         Options options = new Options()
                 .setCreateIfMissing(true);
+
+
 
         String dbPath = "db/rocksdb/data";
 
