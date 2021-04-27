0b0d234ae2385f1192b2bee395ae63361734f0eb
Mark.Kovalyov <mark.kovalyov78@gmail.com>
2021-04-27 23:32:38 +0300

    Changes

--- rust/log-split/src/main.rs
+++ rust/log-split/src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::io::prelude::*;
+use flate2::Compression;
+use flate2::write::ZlibEncoder;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("Usage: log-split [--input FOLDER_NAME] [--date-time-format DATE_TIME_FORMAT] [--granularity {{S|H|D}}] OUTPUT_FOLDER_NAME");
+    } else {
+        println!("Go-go!");
+    }
+    
 }
