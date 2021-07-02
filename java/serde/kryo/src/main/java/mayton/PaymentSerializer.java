65d605c4d17c4fdb9d5aebab271a61afd6a7d940
Mark.Kovalyov <mark.kovalyov78@gmail.com>
2021-07-02 17:58:04 +0300

    Add :bson :kryo

--- java/serde/kryo/src/main/java/mayton/PaymentSerializer.java
+++ java/serde/kryo/src/main/java/mayton/PaymentSerializer.java
@@ -9,11 +9,11 @@ public class PaymentSerializer extends Serializer<Payment> {
 
     @Override
     public void write(Kryo kryo, Output output, Payment payment) {
-
+        output.write(new byte[0]);
     }
 
     @Override
     public Payment read(Kryo kryo, Input input, Class<? extends Payment> aClass) {
-        return null;
+        return new Payment();
     }
 }
