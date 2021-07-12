a10e123fd321552a483c478e881e86a3cd838a9a
Mark.Kovalyov <mark.kovalyov78@gmail.com>
2021-07-12 10:37:59 +0300

    Add :snappy

---
+++ java/probe-snappy/src/main/java/mayton/App.java
@@ -0,0 +1,13 @@
+package mayton;
+
+import org.xerial.snappy.Snappy;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Snappy snappy = new Snappy();
+    }
+}
